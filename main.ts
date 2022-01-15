8b345cc6e04ec4d8600c9fae3f1b55c30c27040b
MichalinaGlab <80153192+MichalinaGlab@users.noreply.github.com>
2022-01-15 10:51:11 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+namespace SpriteKind {
+    export const glowa = SpriteKind.create()
+}
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    diry = -1
+    dirx = 0
+})
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     if (diry == -1) {
         diry = 0
@@ -32,69 +39,89 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     	
     }
 })
-controller.B.onEvent(ControllerButtonEvent.Repeated, function () {
-    if (diry == -1) {
-        diry = 0
-        dirx = 1
-    } else if (diry == 1) {
-        diry = 0
-        dirx = -1
-    } else if (dirx == 1) {
-        diry = 1
-        dirx = 0
-    } else if (dirx == -1) {
-        diry = -1
-        dirx = 0
-    } else {
-    	
-    }
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    diry = 0
+    dirx = -1
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
+function dogenerujjedzonko () {
+    zarelko = sprites.create(img`
+        . . . . 7 7 . . 
+        . . . 7 . . . . 
+        . . 2 2 2 . . . 
+        . 2 2 2 2 2 . . 
+        . 2 2 2 2 2 . . 
+        . 2 2 2 2 2 . . 
+        . . 2 2 2 . . . 
+        . . . . . . . . 
+        `, SpriteKind.Food)
+    zarelko.setPosition(randint(8, maxX - 8), randint(8, maxY - 8))
+}
+sprites.onOverlap(SpriteKind.glowa, SpriteKind.Enemy, function (sprite, otherSprite) {
     game.over(false)
 })
-controller.A.onEvent(ControllerButtonEvent.Repeated, function () {
-    if (diry == -1) {
-        diry = 0
-        dirx = -1
-    } else if (diry == 1) {
-        diry = 0
-        dirx = 1
-    } else if (dirx == 1) {
-        diry = -1
-        dirx = 0
-    } else if (dirx == -1) {
-        diry = 1
-        dirx = 0
-    } else {
-    	
+sprites.onOverlap(SpriteKind.glowa, SpriteKind.Player, function (sprite, otherSprite) {
+    if (ruszamsie == 0) {
+        game.over(false)
     }
 })
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    diry = 0
+    dirx = 1
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    diry = 1
+    dirx = 0
+})
+sprites.onOverlap(SpriteKind.glowa, SpriteKind.Food, function (sprite, otherSprite) {
+    music.pewPew.play()
+    otherSprite.destroy(effects.spray, 500)
+    dogenerujjedzonko()
+    info.changeScoreBy(1)
+    rosnij = 1
+})
+let kamyk_spr: Sprite = null
+let glaz_spr: Sprite = null
 let staryY = 0
 let staryX = 0
+let zarelko: Sprite = null
+let tablica_cialo: Sprite[] = []
 let cialo_spr: Sprite = null
 let diry = 0
 let dirx = 0
-let maxX = 160
-let maxY = 124
+let maxY = 0
+let maxX = 0
+let rosnij = 0
+let ruszamsie = 0
+let przeszkadzajka = randint(20, 100)
+ruszamsie = 1
+rosnij = 0
+maxX = 160
+maxY = 124
 dirx = 1
 diry = 0
 let glowa = image.create(8, 8)
 glowa.fill(7)
-let glowa_spr = sprites.create(glowa, SpriteKind.Player)
+let glowa_spr = sprites.create(glowa, SpriteKind.glowa)
 glowa_spr.setStayInScreen(true)
 let cialko = image.create(8, 8)
 cialko.fill(6)
-let tablica_cialo: Sprite[] = []
 for (let index = 0; index <= 4; index++) {
     cialo_spr = sprites.create(cialko, SpriteKind.Player)
     cialo_spr.setPosition(glowa_spr.x, glowa_spr.y + (index + 1) * 8)
     tablica_cialo.push(cialo_spr)
 }
+dogenerujjedzonko()
 game.onUpdateInterval(500, function () {
+    ruszamsie = 1
     staryX = glowa_spr.x
     staryY = glowa_spr.y
-    tablica_cialo.insertAt(0, tablica_cialo.pop())
-    tablica_cialo[0].setPosition(glowa_spr.x, glowa_spr.y)
+    if (rosnij == 0) {
+        tablica_cialo.insertAt(0, tablica_cialo.pop())
+    } else {
+        cialo_spr = sprites.create(cialko, SpriteKind.Player)
+        tablica_cialo.insertAt(0, cialo_spr)
+        rosnij = 0
+    }
     glowa_spr.x = staryX + dirx * 8
     glowa_spr.y = staryY + diry * 8
     if (glowa_spr.x >= maxX) {
@@ -107,5 +134,41 @@ game.onUpdateInterval(500, function () {
         glowa_spr.y = 0
     } else {
     	
+    }
+    tablica_cialo[0].setPosition(staryX, staryY)
+    ruszamsie = 0
+    przeszkadzajka += -1
+    if (przeszkadzajka <= 0) {
+        przeszkadzajka = randint(20, 100)
+        if (Math.percentChance(50)) {
+            glaz_spr = sprites.create(img`
+                . . . . . . . . 
+                . d d d d d d . 
+                . d e f c e d . 
+                . d c e e c d . 
+                . d f e c f d . 
+                . d e f e c d . 
+                . d d d d d d . 
+                . . . . . . . . 
+                `, SpriteKind.Enemy)
+            glaz_spr.setPosition(randint(8, maxX - 8), randint(8, maxY - 8))
+            glaz_spr.vx = 10
+            glaz_spr.vy = 10
+            glaz_spr.setBounceOnWall(true)
+            glaz_spr.setStayInScreen(true)
+        } else {
+            kamyk_spr = sprites.create(img`
+                . . . . . . . . 
+                . . . . . . . . 
+                . . e f e e . . 
+                . . e e e e . . 
+                . . f e e f . . 
+                . . e f e e . . 
+                . . . . . . . . 
+                . . . . . . . . 
+                `, SpriteKind.Enemy)
+            kamyk_spr.setPosition(randint(8, maxX - 8), randint(8, maxY - 8))
+            kamyk_spr.lifespan = 30000
+        }
     }
 })
